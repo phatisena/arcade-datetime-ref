192d06854e41e63f644051e23e35351bc2cbeb1c
Phatisena <naphatism@hotmail.com>
2025-02-21 17:15:54 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ namespace timeanddate {
     /* 
         This ensures that "time" is checked periodically and event handlers are called.  
     */
-    game.onUpdateInterval(1000, function() {
+    game.onUpdateInterval(864, function() {
         // Only run about every 2 s;  Micro:bit uses a ticker with a 32kHz period, so the count should increase by about 65kHz
         const cpuTime = cpuTimeInSeconds()
         const t = timeFor(cpuTime)
